3d243db92203fcfce0e78256d9fd8d28adcd0adc
Luis_Rebelo <luis_rebelo@dellteam.com>
2021-02-22 08:05:43 -0300

    :lock: Adding in memory users

--- src/main/java/com/pereir4/bankapp/configurations/SecurityConfiguration.java
+++ src/main/java/com/pereir4/bankapp/configurations/SecurityConfiguration.java
@@ -1,8 +1,15 @@
 package com.pereir4.bankapp.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,6 +27,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User.withUsername("admin").password("1234").authorities("admin").build();
+        UserDetails user2 = User.withUsername("user").password("4321").authorities("read").build();
+        userDetailsService.createUser(user1);
+        userDetailsService.createUser(user2);
+
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
