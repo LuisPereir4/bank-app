87e254ab04e401c3be97dfc319000a8c345c7db0
Luis_Rebelo <luis_rebelo@dellteam.com>
2021-02-23 09:50:02 -0300

    :wrench: Configuring H2 in memory database

--- src/main/java/com/pereir4/bankapp/configurations/SecurityConfiguration.java
+++ src/main/java/com/pereir4/bankapp/configurations/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    // Determining which endpoints should be secure
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,6 +31,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    // Ignoring security in memory db management endpoints
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    // Creating users in memory (Temporary)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
@@ -41,6 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
     }
 
+    // Providing mandatory password encoder bean
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
